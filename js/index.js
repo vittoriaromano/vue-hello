730e77740101bf6eb06d2ce0a84300458e7c33dd
vittoriaromano <vittoria5005@gmail.com>
2023-03-13 18:21:47 +0400

    inserimento-h1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const { createApp } = Vue
+    //option api
+  const vm = createApp({
+    data() {
+      return {
+        //qui inseriamo i dati che voglio utilizzare
+        message: ' VUE.JS ',
+      }
+    },
+  }).mount('#app');
